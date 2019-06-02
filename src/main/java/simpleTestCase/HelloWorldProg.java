5d5d8d036ca0592451f4ebb545c11bbd911e58fb
abha <abha@example.com>
2019-06-02 15:40:21 +0530

    4th step

--- src/main/java/simpleTestCase/HelloWorldProg.java
+++ src/main/java/simpleTestCase/HelloWorldProg.java
@@ -1,11 +1,18 @@
 package simpleTestCase;
 
+import org.testng.annotations.Test;
+
 public class HelloWorldProg {
 
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		
      System.out.println("Hello World");
      System.out.println("Agins hello world");
-	}
+	}*/
 
+	@Test
+    public void test_TeamCity() {
+		System.out.println("Hello World");
+	}
+	
 }
