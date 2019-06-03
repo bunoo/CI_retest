6e98ab29433b65d8f4ddf91cb5e1cb895a2b70df
abha <abha@example.com>
2019-06-03 15:25:39 +0530

    Added a unit test with @Test annotation under src\test\java folder

---
+++ src/test/java/unitTestCases/MyFirstUnitTest.java
@@ -0,0 +1,11 @@
+package unitTestCases;
+
+import org.testng.annotations.Test;
+
+public class MyFirstUnitTest {
+
+	@Test
+	public void unitTest1() {
+		System.out.println("My first unit test case");
+	}
+}
